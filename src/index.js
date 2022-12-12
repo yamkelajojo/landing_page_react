cd2f347547db617a37abc40d0a904273a22075e4
Dojo <mhlengijojo7@gmail.com>
2022-12-12 09:00:36 +0200

    Nav with hero

--- src/index.js
+++ src/index.js
@@ -4,18 +4,13 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/header';
-import "./components/bootstrap/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./components/custom.css";
+import "./scss/style.scss"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Fragment>
-    <div style={{height : "100vh"}} className='d-flex flex-column'>
       <Header></Header>
-    </div>
   </React.Fragment>
 );
 
